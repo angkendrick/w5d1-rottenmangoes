20ee5a2805ce3e21b1aaab1c958eb3fd1cb4113d
Ken Ang <angkendrick@gmail.com>
2015-07-27 15:54:22 -0400

    end part 10 sessions

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,6 @@ Rails.application.routes.draw do
 
   resources :users, only: [:new, :create]
 
-  resources :sessions, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
 
 end
