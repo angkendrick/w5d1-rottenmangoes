da666e175050746da4e272097bf4ef02e6ec44fb
Ken Ang <angkendrick@gmail.com>
2015-07-27 16:40:30 -0400

    end part 15

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,11 +1,15 @@
 class ReviewsController < ApplicationController
+
+  before_filter :load_movie
+  before_filter :restrict_access
+
   def new
-    @movie = Movie.find(params[:movie_id]) #movie_id bec this is called inside reviews(?)
+    #@movie = Movie.find(params[:movie_id]) #movie_id bec this is called inside reviews(?)
     @review = @movie.reviews.build
   end
 
   def create
-    @movie = Movie.find(params[:movie_id])
+   #@movie = Movie.find(params[:movie_id])
     @review = @movie.reviews.build(review_params)
     @review.user_id = current_user.id
 
@@ -17,6 +21,10 @@ class ReviewsController < ApplicationController
   end
 
   protected
+
+  def load_movie
+    @movie = Movie.find(params[:movie_id])
+  end
 
   def review_params
     params.require(:review).permit(:text, :rating_out_of_ten)
